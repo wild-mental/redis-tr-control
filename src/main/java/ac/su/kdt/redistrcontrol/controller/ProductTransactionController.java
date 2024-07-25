8719ebb99fda0cefa2d2748abda4a9af408e41f5
wild-mental <wild.mental@kidsnote.com>
2024-07-25 16:14:58 +0900

    SetIfAbsentGetIfPresent 로직 커밋 (예제 미완성)

--- src/main/java/ac/su/kdt/redistrcontrol/controller/ProductTransactionController.java
+++ src/main/java/ac/su/kdt/redistrcontrol/controller/ProductTransactionController.java
@@ -44,18 +44,24 @@ public class ProductTransactionController {
                 return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
+        // 이미 요청된 상품 생성 Transaction Key 가 재수신된 경우 에러 응답!
+        // -> 에러응답으로는 충분하지 않다!
+        // => 고객의 에러 페이지 수신 시 뒤로가기 및 재호출을 수많은 횟수 반복하면서 결국 TTL 초과하며 중복 요청 발생 가능
         return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
 
     // Post 요청 전 TransactionKey 발급된 것을 전제로 함
-    @PostMapping("/with-cache")
+    @PostMapping("/with-cache1")
     public ResponseEntity<Product> createProduct2(
         @RequestParam(name = "transaction-key") String transactionKey,
         @RequestBody ProductForm product
     ) {
         // 요청 수신 후, transactionKey 부터 검사
         Optional<Product> createdProduct = redisService.setIfAbsentGetIfPresent(
-            transactionKey, LocalDateTime.now().toString()
+            // 복잡도가 높아서 다른 코드 흐름으로 대체
+            transactionKey,
+            // LocalDateTime.now().toString()
+            product.toEntity()
         );
         if (createdProduct.isEmpty()) {
             try {
@@ -67,4 +73,5 @@ public class ProductTransactionController {
         }
         return new ResponseEntity<>(createdProduct.get(), HttpStatus.CREATED);
     }
+
 }
--- src/main/java/ac/su/kdt/redistrcontrol/service/RedisService.java
+++ src/main/java/ac/su/kdt/redistrcontrol/service/RedisService.java
@@ -3,6 +3,7 @@ package ac.su.kdt.redistrcontrol.service;
 import ac.su.kdt.redistrcontrol.domain.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -28,23 +29,26 @@ public class RedisService {
 
     // TODO : ProductTransactionService 로 이동
     // DB 캐싱을 겸하는 Transaction 검사용 메서드
-    public Optional<Product> setIfAbsentGetIfPresent(String key, String value) {
-        // 1) Object -> JSON String 언파싱
-        boolean isSet = Boolean.TRUE.equals(  // null 값까지 커버하도록 핸들링
+    public Optional<Product> setIfAbsentGetIfPresent(String key, Product value) {
+        // 1) Object -> JSON String 언파싱 수행
+        String jsonValue;
+        //
+        boolean isSet = Boolean.TRUE.equals(
             redisTemplateDb0.opsForValue().setIfAbsent(
                 key,
-                value,  // 사용 시나리오에 따라서 키 값을 아무 값이나 쓰지 않고 실제 Caching 데이터로 쓰면 좋다
-                Duration.ofSeconds(10)  // 사용 시나리오에 따라 적절히 조정
+                "",
+                // jsonValue,  // Product 객체를 JSON String 으로 저장
+                Duration.ofSeconds(300)  // 10분간 재사용 가능한 캐시 유지
             )
         );
         if (isSet) {
-            redisTemplateDb0.opsForValue().get(key);
             // 캐시에서 받아온 값을 응답 (String 을 Product 로 파싱)
             // 2) JSON String -> Object 파싱
+            String cachedProductString = redisTemplateDb0.opsForValue().get(key);
+            Product cachedProduct;
             return Optional.of(
-                new Product(
-                    // Create 에 해당하는 데이터 채워주기
-                )
+                new Product()
+                // cachedProduct
             );  // Product 객체 형태로 반환
         }
         // 현재 요청이 최초 요청이므로 바로 생성
