511ba07f9758188c6dea864eb09f0942ce4ac231
wild-mental <wild.mental@kidsnote.com>
2024-07-28 18:38:04 +0900

    RedisService 코드 정리 1) 기본 메서드 주석 정리 2) Product 캐싱용 메서드 주석 정리 3) Object 캐싱용 메서드 추가 및 주석 정리 4) 메서드 순서 정리 5) 사용하지 않는 메서드 주석 처리

--- src/main/java/ac/su/kdt/redistrcontrol/service/RedisService.java
+++ src/main/java/ac/su/kdt/redistrcontrol/service/RedisService.java
@@ -4,20 +4,36 @@ import ac.su.kdt.redistrcontrol.domain.Product;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.util.Optional;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class RedisService {
     private final StringRedisTemplate redisTemplateDb0;
-    private final StringRedisTemplate redisTemplateDb1;
-    private final StringRedisTemplate redisTemplateDb2;
+    private final RedisTemplate<String, Object> redisObjTemplateDb0;
     private final ObjectMapper objectMapper;
 
+    // ####################################################
+    // 1) Transaction 검사용 기본 메서드
+    // ####################################################
+    public boolean setIfAbsent(String key, String value) {
+        return Boolean.TRUE.equals(  // null 값까지 커버하도록 핸들링
+            redisTemplateDb0.opsForValue().setIfAbsent(
+                key,
+                value,  // 사용 시나리오에 따라서 키 값을 아무 값이나 쓰지 않고 실제 Caching 데이터로 쓰면 좋다
+                Duration.ofSeconds(10)  // 사용 시나리오에 따라 적절히 조정
+            )
+        );
+    }
+
+    // ####################################################
+    // 2) Product 데이터 캐싱을 위한 setter, getter 메서드
+    // ####################################################
     public boolean setProduct(String transactionKey, Product product) {
         try {
             String productJsonString = objectMapper.writeValueAsString(product);
@@ -40,42 +56,55 @@ public class RedisService {
         }
     }
 
-    // Transaction 검사용 메서드로 사용
-    public boolean setIfAbsent(String key, String value) {
-        return Boolean.TRUE.equals(  // null 값까지 커버하도록 핸들링
-            redisTemplateDb0.opsForValue().setIfAbsent(
-                key,
-                value,  // 사용 시나리오에 따라서 키 값을 아무 값이나 쓰지 않고 실제 Caching 데이터로 쓰면 좋다
-                Duration.ofSeconds(10)  // 사용 시나리오에 따라 적절히 조정
+    // ####################################################
+    // 3) Entity 데이터 캐싱을 위한 범용화된 setter, getter 메서드
+    // ####################################################
+    public boolean setObject(String transactionKey, Object object) {
+        return Boolean.TRUE.equals(
+            redisObjTemplateDb0.opsForValue().setIfAbsent(
+                transactionKey, object
             )
         );
     }
 
-    // TODO : ProductTransactionService 로 이동
-    // DB 캐싱을 겸하는 Transaction 검사용 메서드
-    public Optional<Product> setIfAbsentGetIfPresent(String key, Product value) {
-        // 1) Object -> JSON String 언파싱 수행
-        String jsonValue;
-        //
-        boolean isSet = Boolean.TRUE.equals(
-            redisTemplateDb0.opsForValue().setIfAbsent(
-                key,
-                "",
-                // jsonValue,  // Product 객체를 JSON String 으로 저장
-                Duration.ofSeconds(300)  // 10분간 재사용 가능한 캐시 유지
-            )
-        );
-        if (isSet) {
-            // 캐시에서 받아온 값을 응답 (String 을 Product 로 파싱)
-            // 2) JSON String -> Object 파싱
-            String cachedProductString = redisTemplateDb0.opsForValue().get(key);
-            Product cachedProduct;
-            return Optional.of(
-                new Product()
-                // cachedProduct
-            );  // Product 객체 형태로 반환
+    public Object getObject(String transactionKey, Object object) {
+        Object cachedObj = redisObjTemplateDb0.opsForValue().get(transactionKey);
+        try {
+            return objectMapper.readValue(
+                Objects.requireNonNull(cachedObj).toString(),
+                object.getClass()
+            );
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
-        // 현재 요청이 최초 요청이므로 바로 생성
-        return Optional.empty();
     }
+
+    // ####################################################
+    // 4) DB 캐싱을 겸하는 Transaction 검사용 메서드
+    // ####################################################
+//    public Optional<Product> setIfAbsentGetIfPresent(String key, Product value) {
+//        // 1) Object -> JSON String 언파싱 수행
+//        String jsonValue;
+//        //
+//        boolean isSet = Boolean.TRUE.equals(
+//            redisTemplateDb0.opsForValue().setIfAbsent(
+//                key,
+//                "",
+//                // jsonValue,  // Product 객체를 JSON String 으로 저장
+//                Duration.ofSeconds(300)  // 10분간 재사용 가능한 캐시 유지
+//            )
+//        );
+//        if (isSet) {
+//            // 캐시에서 받아온 값을 응답 (String 을 Product 로 파싱)
+//            // 2) JSON String -> Object 파싱
+//            String cachedProductString = redisTemplateDb0.opsForValue().get(key);
+//            Product cachedProduct;
+//            return Optional.of(
+//                new Product()
+//                // cachedProduct
+//            );  // Product 객체 형태로 반환
+//        }
+//        // 현재 요청이 최초 요청이므로 바로 생성
+//        return Optional.empty();
+//    }
 }
