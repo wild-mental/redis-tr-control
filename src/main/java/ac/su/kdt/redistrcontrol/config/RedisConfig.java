56b1ea69129d8a63374d3ab4e32a592e410ebe16
wild-mental <wild.mental@kidsnote.com>
2024-07-28 18:33:50 +0900

    RedisConfig 에 Jackson 활용 Object 기록을 위한 Generic 템플릿 선언

--- src/main/java/ac/su/kdt/redistrcontrol/config/RedisConfig.java
+++ src/main/java/ac/su/kdt/redistrcontrol/config/RedisConfig.java
@@ -1,5 +1,9 @@
 package ac.su.kdt.redistrcontrol.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +12,9 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -76,8 +83,24 @@ public class RedisConfig {
 
     @Bean
     public StringRedisTemplate redisTemplateDb0() {
-        StringRedisTemplate redisTemplate = new StringRedisTemplate();
+        return new StringRedisTemplate(redisConnectionFactory0());
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisObjTemplateDb0() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory0());
+
+        // Custom ObjectMapper 설정
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        // Value Serializer 설정
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+        redisTemplate.setValueSerializer(serializer);
+
         return redisTemplate;
     }
 
