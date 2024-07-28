a1c2f938b426a8940dffac519ddf9d36604872a4
wild-mental <wild.mental@kidsnote.com>
2024-07-28 22:04:45 +0900

    CategoryResponseDTO 주석 정리

--- src/main/java/ac/su/kdt/redistrcontrol/domain/CategoryResponseDTO.java
+++ src/main/java/ac/su/kdt/redistrcontrol/domain/CategoryResponseDTO.java
@@ -14,9 +14,15 @@ public class CategoryResponseDTO {
     private String name;
     private int depth;  // 최상위 1, 그 아래부터 2, 3, 4, ...
 
-    // 1) 가장 단순한 형태로 참조 문제 해결하기 (객체 사용 포기)
+    // 순환참조 해결 기본 전략 #################
+    // -> 순환참조가 일어나는 필드를 포함하지 않게끔
+    //    1) Long (id 값)
+    //    2) String (toString() 호출값)
+    //    3)Map<>
+    // 등으로 핸들링 #########################
+
+    // 전략 1) 가장 단순한 형태로 참조 문제 해결하기 (객체 사용 포기)
     private Long parentId;  // 상위 카테고리 객체 타입이 아니라 id 로만 다루기
-    // 순환참조가 일어나는 필드를 포함하지 않게끔만 Long, String(toString()), Map<>
 
     public static CategoryResponseDTO fromCategory(Category category) {
         return new CategoryResponseDTO(
@@ -28,24 +34,9 @@ public class CategoryResponseDTO {
         );
     }
 
-    // 2) 현재 카테고리에 이르기까지 상위 항목만 리스트로 응답 (객체사용 -> 순환참조 재개됨)
-//    private List<Category> parents;
-//
-//    public static CategoryResponseDTO fromCategoryWithParentsList(Category category) {
-//        return new CategoryResponseDTO(
-//            category.getId(),
-//            category.getName(),
-//            category.getDepth(),
-//            category.getParent() != null ? category.getParent().getId() : null,
-//            category.getParentsAsc(),
-//        );
-//    }  // 응답 포맷이 깨지므로 사용 불가
+    // 전략 2) 단일 String 값으로 다루기 -> 예제 생략
 
-    // 3) 리스트 응답시 REST API 설계를 고려해서 응답한 결과로 바로 페이지 호출 가능하도록
-    //    URL 형태 (String) 응답 (HyperLink 연속성 추구)
-    //    - Depth 를 List index 로 대체 (추가 필드 X)
-    //    - 실제 categoryId(후속 요청)를 URL 에 반영 (String)
-    //    - 카테고리 이름도 반환 (String)
+    // 전략 3) Entity 이름과 URL 을 포함하는 Map<> 자료형으로 다루기
     private List<Map<String, String>> parentsUrl;
 
     public static CategoryResponseDTO fromCategoryWithParentsUrlList(Category category) {
@@ -56,5 +47,5 @@ public class CategoryResponseDTO {
             category.getParent() != null ? category.getParent().getId() : null,
             category.getParentsMapAsc()
         );
-    }  // 응답 포맷이 깨지므로 사용 불가
+    }
 }
